e22ae70e7018dcd800cf42341ffbab88a30e856e
Iskren.Stanislavov <iskren.jgd@gmail.com>
2015-04-22 09:56:24 +0300

    skeleton

--- js/main.js
+++ js/main.js
@@ -3,23 +3,40 @@ require.config({
 });
 
 define(function(require) {
-	var PIXI 		= require("libs/pixi");
-	var Ball        = require("ball");
-	var config      = require("config");
+    var PIXI        = require("libs/pixi");
+    var config      = require("config");
 
-	var stage = new PIXI.Stage(0x39435E);
-	var ball = stage.addChild(new Ball());
+    var stage = new PIXI.Stage(0x39435E);
     var renderer = PIXI.autoDetectRenderer(config.canvas.width, config.canvas.height, {
-    	"roundPixels": true,
-    	"antialias": true,
-//     	"transparent": true,
-    	"view": document.getElementById(config.canvas.id)});
+        "roundPixels": true,
+        "antialias": true,
+//      "transparent": true,
+        "view": document.getElementById(config.canvas.id)});
+
     document.body.appendChild(renderer.view);
     requestAnimFrame(animate);
-	function animate() {
-		ball.move();
-	    renderer.render(stage);
-	    requestAnimFrame(animate);
-	}
+    function animate() {
+        renderer.render(stage);
+        // requestAnimFrame(animate);
+    }
+
+    var loader = new PIXI.AssetLoader([
+        "images/cards-faces-classic.png",
+        "images/cards_back.png",
+        ]);
+
+    loader.onComplete = function(){
+        var card = { x:10, y:1, w: 100, h:100};
+        var face = stage.addChild(new cards.CardFace(card.x, card.y, card.w, card.h));
+        face.x = 100;
+        var back = stage.addChild(new cards.CardBack());
+        back.x = 200;
+        requestAnimFrame(animate);
+
+    };
+
+    loader.load();
+
+
 
 });
